9d32e21c0444d5c54da8ebab820fb93d378a8165
TIWARI ROSHAN AJIT <156943356+RoshanTiwari07@users.noreply.github.com>
2025-09-26 15:00:10 +0530

    Refactor SonarQube analysis function with credentials

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,12 @@
-def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
+def call(String sonarQubeServer, String projectName, String projectKey) {
+    withCredentials([string(credentialsId: 'Sonar', variable: 'SONAR_TOKEN')]) {
+        withSonarQubeEnv(sonarQubeServer) {
+            sh """
+                ${SONAR_HOME}/bin/sonar-scanner \
+                -Dsonar.projectName=${projectName} \
+                -Dsonar.projectKey=${projectKey} \
+                -Dsonar.token=\${SONAR_TOKEN}
+            """
+        }
+    }
 }
